a0a512a31b71c6e3bbe6d0f9e69819b09e80d697
isaac nkanta <170721134+nkanta99@users.noreply.github.com>
2024-07-23 15:59:22 +0100

    feature: added middleware auth.js

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -3,11 +3,13 @@ const User = require("./model");
 const postTest = async (req, res) => {
     console.log("postTest controller ")
 
-    const user = await User.create(req.body);
-    res.status(201).json({message: "success", user: user});
-
-    
-}
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ message: "success", user: user });
+    } catch (error) {
+       res.status(501).json({message: error.message, error: error}); 
+    }   
+};
 
 
 module.exports = {
